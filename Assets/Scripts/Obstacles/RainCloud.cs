3eca137b3db8e759d5df332e2331c42d6515e2d6
cole-b216 <coleconnors987@gmail.com>
2026-07-08 01:46:59 -0400

    Add working Rain Cloud, improve behavior
    - Polished RainCloud prefab with interactions between 3 states
    - Raining clouds will return to idle after not being touched for a set time
    - Vapor will head back to the cloud's origin

--- Assets/Scripts/Obstacles/RainCloud.cs
+++ Assets/Scripts/Obstacles/RainCloud.cs
@@ -1,4 +1,5 @@
 using System.Xml;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RainCloud : BasicCloud
@@ -9,8 +10,9 @@ public class RainCloud : BasicCloud
      *      - The player collides with the cloud, and it begins 'raining'
      *      - While the player is connected to the cloud the rain timer goes down
      *      - It will turn into vapor and you cannot interact with it until it respawns
+     *      - There is a rain timer before it goes back to idle.
      * I would like to discuss/flesh out some of the desired behavior. Right now, it cumulates the total time 
-     * that a player has been on the cloud. Maybe it should reset after the player has been off it for a while?
+     * that a player has been on the cloud. Do we want it to reset after the player has been off it for a while?
      */
 
     private enum RainCloudStates { Idle, Raining, Vapor } // Idle = full cloud. Raining = player is on the cloud. Vapor = waiting to respawn.
@@ -18,12 +20,15 @@ public class RainCloud : BasicCloud
     [SerializeField] private Sprite rainingCloudSprite;
     [SerializeField] private Sprite fadedVaporSprite;
 
-    [SerializeField] private float rainDuration = 5f;
-    [SerializeField] private float vaporDuration = 3f;
+    [SerializeField] private float rainDuration = 3f;
+    [SerializeField] private float vaporDuration = 4f;
+    [SerializeField] private float resetTimer = 3f;
 
     private RainCloudStates state = RainCloudStates.Idle;
     private float timer = 0f; // This will count how long a state has been active 
+    private float offTimer = 0f; // This one counts how long the player has been off, in order to reset the rain.
     // **Update: timer has a problem b/c the player bounces off at the top of a jump. Switched to cumulative method.
+
 
     protected override void Start()
     {
@@ -46,6 +51,7 @@ public class RainCloud : BasicCloud
                 {
                     state = RainCloudStates.Raining;
                     timer = 0f;
+                    offTimer = 0f;
                     sr.sprite = rainingCloudSprite; // Idle -> Raining
                 }
                 break;
@@ -53,10 +59,19 @@ public class RainCloud : BasicCloud
             case RainCloudStates.Raining:
                 if (isCollidingPlayer)
                 {
+                    offTimer = 0f;
                     timer += Time.deltaTime;
                     if (timer >= rainDuration)
                     {
                         StartVapor();
+                    }
+                }
+                else
+                {
+                    offTimer += Time.deltaTime;
+                    if (offTimer >= resetTimer)
+                    {
+                        Respawn(); // Might want to change this to a new function when we have animations and sprites
                     }
                 }
                 break;
@@ -68,7 +83,6 @@ public class RainCloud : BasicCloud
                     Respawn(); 
                 }
                 break;
-
         }
     }
 
@@ -77,6 +91,7 @@ public class RainCloud : BasicCloud
     {
         if (state == RainCloudStates.Vapor)
         {
+            ReturnToPos();
             return;
         }
         base.FixedUpdate();
@@ -88,17 +103,16 @@ public class RainCloud : BasicCloud
         // Raining -> Vapor
         state = RainCloudStates.Vapor;
         timer = 0f;
+        sr.sprite = fadedVaporSprite;
 
-        isCollidingPlayer = false; // Has to be false, collision is turning off
+        // Turning off player-cloud collision for this state
+        isCollidingPlayer = false; 
         playerRB = null;
-
         if (col != null)
         {
             col.enabled = false;
-        }
-        sr.sprite = fadedVaporSprite; 
-        rb.linearVelocityY = 0f;
-}
+        } 
+    }
 
 
     private void Respawn()
@@ -106,12 +120,24 @@ public class RainCloud : BasicCloud
         // Vapor -> Idle
         state = RainCloudStates.Idle;
         timer = 0f;
-        rb.position = new Vector2(rb.position.x, startY); // What do we want to do with the position? Should we make it smooth back to start or stay in place?
-        rb.linearVelocityY = 0f;
         sr.sprite = idleCloudSprite; 
-        if (col  != null) // Back to enabled collision
+
+        if (col != null) // Back to enabled collision
         {
             col.enabled = true;
+        }
+    }
+
+
+    private void ReturnToPos()
+    {
+        if (rb.position.y >= startY)
+        {
+            rb.linearVelocityY = speed * -1; // Start moving down once cloud has reached certain height
+        }
+        else if (rb.position.y <= startY)
+        {
+            rb.linearVelocityY = 0; // Stop moving down if cloud is too low
         }
     }
 }
