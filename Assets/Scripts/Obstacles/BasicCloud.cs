db27a58500bf412ee19e995d99472bfe856e8185
jsj84-cornell <jasonjang777@gmail.com>
2026-07-07 20:12:03 -0400

    Cloud bobbing script

--- Assets/Scripts/Obstacles/BasicCloud.cs
+++ Assets/Scripts/Obstacles/BasicCloud.cs
@@ -1,25 +1,70 @@
+using System;
 using UnityEngine;
 
 public class BasicCloud : MonoBehaviour
 {
+    [SerializeField] private float speed = 2f; // How fast the cloud should bob up & down
+    [SerializeField] private float maxDistance = 3f; // How far the cloud moves up
+    [SerializeField] private float boostThreshold = 2f; // Where the player can get a boost off the cloud
     private bool isCollidingPlayer;
+
+    private Rigidbody2D playerRB;
+    private Rigidbody2D rb;
+    private float startY; // Cloud's starting y position
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      rb = GetComponent<Rigidbody2D>();
+      startY = rb.position.y;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    // void Update()
+    // {
         
+    // }
+
+    void FixedUpdate()
+    {
+        if (rb.position.y >= startY + maxDistance)
+        {
+            rb.linearVelocityY = speed * -1; // Start moving down once cloud has reached certain height
+        }
+        else if (rb.position.y <= startY)
+        {
+            rb.linearVelocityY = 0; // Stop moving down if cloud is too low
+        }
+
+        if (rb.position.y <= startY && playerRB != null && isCollidingPlayer 
+        && playerRB.position.y > rb.position.y)
+        {
+            rb.linearVelocityY = speed; // Start moving up if player is on top of it
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.CompareTag("Player"))
         {
-            // Start bouncing coroutine?
+            isCollidingPlayer = true;
+            if (playerRB == null)
+            {
+                playerRB = other.gameObject.GetComponent<Rigidbody2D>(); // Get reference to player
+            }
         }
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            isCollidingPlayer = false;
+        }
+    }
+
+    public bool isBoostAvailable() // Whether the player gets a boost from jumping off the cloud
+    {
+        // If the cloud is moving up, and it's above the boost threshold distance, then
+        // the player can get a boost
+        return (rb.linearVelocity.y >= 0 && rb.position.y >= startY + boostThreshold);
     }
 }
