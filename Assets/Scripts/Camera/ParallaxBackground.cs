1de61ae7bdf002328eb993edc18ada25e99d675b
Annabelle <annabelle.ford.314@gmail.com>
2026-07-10 09:19:52 +0800

    parallax testing

--- Assets/Scripts/Camera/ParallaxBackground.cs
+++ Assets/Scripts/Camera/ParallaxBackground.cs
@@ -2,19 +2,22 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    private float startPos;
+    private float startPos, length;
     // 0-1 (0=no effect, 1=normal, 0.5=half effect)
     public float parallaxEffect;
 
     void Start()
     {
         startPos = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
     void FixedUpdate()
     {
         float distance = GameManager.Instance.GetCamera().transform.position.y
-            + parallaxEffect;
+            * parallaxEffect;
+        float movement = GameManager.Instance.GetCamera().transform.position.y
+            * (1 - parallaxEffect);
         transform.position = new Vector3(transform.position.x, startPos + distance, transform.position.z);
     }
 }
