ca67b3b9c185bedab5416ab5b89474b4705b5615
jaredrojas08 <jaredrojas08@gmail.com>
2026-07-13 21:52:58 -0400

    Made player combo
    Signed-off-by: jaredrojas08 <jaredrojas08@gmail.com>

---
+++ Assets/Scripts/Player/PlayerCombo.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerCombo : MonoBehaviour
+{
+
+    private TMP_Text comboText;
+
+    private void Awake()
+    {
+        comboText = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int combo = GameManager.Instance.Combo;
+        comboText.text = $"{combo}";
+    }
+}
