7d296eee5d7591ae0664b97e6419eb94eb26008b
cole-b216 <coleconnors987@gmail.com>
2026-07-15 20:36:15 -0400

    Clean script
    - Cleaned the CosmicRay script of old code

--- Assets/Scripts/Obstacles/CosmicRay.cs
+++ Assets/Scripts/Obstacles/CosmicRay.cs
@@ -1,223 +1,3 @@
-//using System.Collections;
-//using Unity.VisualScripting;
-//using UnityEngine;
-
-//public class CosmicRay : MonoBehaviour
-//{
-//    public SpriteRenderer warning;
-//    public SpriteRenderer ray;
-
-//    public Camera ourCamera;
-//    public float rayWidth = 1f;
-//    public float topGap = 0.5f;
-//    public float warningTempFix = 0.5f; // Temporary offset fixes before having sprites/animations
-
-//    public float warningDuration = 2.5f;
-//    public float rayDropDuration = 0.1f;
-//    public float activeDuration = 2.0f;
-
-//    public float warningBlinkSlow = 0.3f;
-//    public float warningBlinkFast = .03f;
-
-//    private BoxCollider2D killZoneCollider;
-//    private float rayHeight;
-
-//    private float posWarningY;
-//    private float posTopY;
-
-//    // Currently using the square sprite, but idk what best practice is or sprite size? I added this scaling solution
-//    // but we can remove it later if it causes any problems or if we can simplify this.
-//    private float rayNativeWidth = 1f;
-//    private float rayNativeHeight = 1f;
-//    private float rayScaleX = 1f;
-
-
-//    void Awake()
-//    {
-//        killZoneCollider = GetComponent<BoxCollider2D>();
-//        killZoneCollider.enabled = false;
-//        killZoneCollider.isTrigger = true;
-//        ourCamera = Camera.main; 
-
-//        if ( warning != null )
-//        {
-//            warning.gameObject.SetActive(false);
-//        }
-
-//        if ( ray != null )
-//        {
-//            ray.gameObject.SetActive(false);
-
-//            if (ray.sprite != null)
-//            {
-//                // Part of the scaling solution w/ world units
-//                rayNativeWidth = ray.sprite.bounds.size.x;
-//                rayNativeHeight = ray.sprite.bounds.size.y;
-//                rayScaleX = rayWidth / rayNativeWidth;
-//            }
-//        }
-//    }
-
-
-//    void OnEnable()
-//    {
-//        DesignRay();
-//        StartCoroutine(RunRaySequence());
-//    }
-
-
-//    private IEnumerator RunRaySequence()
-//    {
-//        // WARNING 
-//        yield return WarningSequence();
-
-//        // RAY (DROP)
-//        yield return RayDropSequence();
-
-//        // RAY (ACTIVE)
-//        yield return RayActiveSequence();
-
-//        // REMOVE
-//        RemoveRay();
-
-//    }
-
-
-//    private void DesignRay()
-//    {
-//        if (ourCamera == null)
-//        {
-//            return;
-//        }
-
-//        transform.SetParent(ourCamera.transform, true);
-
-//        float cameraHalfHeight = ourCamera.orthographicSize;
-
-//        float topOffset = cameraHalfHeight + topGap;
-//        float botOffset = -cameraHalfHeight;
-//        float warningOffset = cameraHalfHeight - warningTempFix;
-
-//        rayHeight = topOffset - botOffset;
-//        float centerOffset = (topOffset + botOffset) / 2f;
-
-//        Vector3 pos = transform.localPosition;
-//        pos.y = centerOffset;
-//        transform.localPosition = pos;
-
-//        posWarningY = warningOffset - centerOffset;
-//        posTopY = topOffset - centerOffset;
-
-//        killZoneCollider.size = new Vector2(rayWidth, rayHeight);
-//        killZoneCollider.offset = Vector2.zero;
-//    }
-
-
-//    private IEnumerator WarningSequence()
-//    {
-//        if (warning != null)
-//        {
-//            warning.gameObject.SetActive(true);
-//            Vector3 wPos = warning.transform.localPosition;
-//            wPos.y = posWarningY;
-//            warning.transform.localPosition = wPos;
-//        }
-
-//        float timer = 0f;
-//        float blinkTimer = 0f;
-//        bool visible = true;
-
-//        while (timer < warningDuration)
-//        {
-//            timer += Time.deltaTime;
-//            blinkTimer += Time.deltaTime;
-
-//            // Set up a lerp so any warning sprite will blink. Should it be animated or just a sprite?
-
-//            float transitionBlink = timer / warningDuration;
-//            float currentBlink = Mathf.Lerp(warningBlinkSlow, warningBlinkFast, transitionBlink);
-
-//            if (blinkTimer >= currentBlink)
-//            {
-//                blinkTimer = 0f;
-//                visible = !visible;
-//                if (warning != null)
-//                {
-//                    warning.enabled = visible;
-//                }
-//            }
-//            yield return null;
-//        }
-
-//       // set null ? do we want it to vanish?
-//    }
-
-
-//    private IEnumerator RayDropSequence()
-//    {
-//        if (ray != null)
-//        {
-//            ray.gameObject.SetActive(true);
-//            Vector3 dPos = ray.transform.localPosition;
-//            dPos.y = posTopY;
-//            ray.transform.localPosition = dPos;
-//            ray.transform.localScale = new Vector3(rayScaleX, 0f, 1f);
-//        }
-
-//        float currentDropTime = 0f;
-//        while (currentDropTime < rayDropDuration)
-//        {
-//            currentDropTime += Time.deltaTime;
-//            float unitTransition = Mathf.Clamp01(currentDropTime / rayDropDuration);
-//            float currentHeight = rayHeight * unitTransition;
-//            if (ray != null)
-//            {
-//                // Scaling solution
-//                float currentScaleY = currentHeight / rayNativeHeight;
-//                Vector3 dPos = ray.transform.localPosition;
-//                dPos.y = posTopY - (currentHeight / 2);
-//                ray.transform.localPosition = dPos;
-//                ray.transform.localScale = new Vector3(rayScaleX, currentScaleY, 1f);
-
-//            }
-//            yield return null;
-//        }
-//    }
-
-
-//    private IEnumerator RayActiveSequence()
-//    {
-//        killZoneCollider.enabled = true;
-
-//        float uptime = 0f;
-//        while (uptime < activeDuration)
-//        {
-//            uptime += Time.deltaTime;
-//            yield return null;
-//        }
-//    }
-
-
-//    private void RemoveRay()
-//    {
-//        killZoneCollider.enabled = false;
-//        if (ray != null)
-//        {
-//            ray.gameObject.SetActive(false);
-//        }
-//        Destroy(gameObject);
-//    }
-
-
-//    private void OnTriggerEnter2D(Collider2D other)
-//    {
-//        if (other.CompareTag("Player") && GameManager.Instance != null)
-//        {
-//            GameManager.Instance.PlayerDeath();
-//        }
-//    }
-
-//}
 using System.Collections;
 using UnityEngine;
 
@@ -226,11 +6,11 @@ public class CosmicRay : MonoBehaviour
     public SpriteRenderer warning;
     public SpriteRenderer ray;
     public ParticleSystem topParticles;
-
     public Camera ourCamera;
+
     public float rayWidth = 1f;
     public float topGap = 0.5f;
-    public float warningTempFix = 0.5f; // Temporary offset fixes before having sprites/animations
+    public float warningTempFix = 0.5f;
 
     public float warningDuration = 2.5f;
     public float activeDuration = 2.0f;
@@ -239,18 +19,15 @@ public class CosmicRay : MonoBehaviour
     public float warningBlinkFast = .03f;
 
     private BoxCollider2D killZoneCollider;
-    private float rayHeight;
     public Animator rayAnimator;
-
+    
+    private float rayHeight;
     private float posWarningY;
     private float posTopY;
-
     private float posBotY;
     private float topOffsetPivot = 0f;
     private float fullScaleY;
 
-    // Currently using the square sprite, but idk what best practice is or sprite size? I added this scaling solution
-    // but we can remove it later if it causes any problems or if we can simplify this.
     private float rayNativeWidth = 1f;
     private float rayNativeHeight = 1f;
     private float rayScaleX = 1f;
@@ -313,7 +90,6 @@ public class CosmicRay : MonoBehaviour
 
         // REMOVE
         RemoveRay();
-
     }
 
 
@@ -369,7 +145,7 @@ public class CosmicRay : MonoBehaviour
             timer += Time.deltaTime;
             blinkTimer += Time.deltaTime;
 
-            // Set up a lerp so any warning sprite will blink. Should it be animated or just a sprite?
+            // Set up a lerp so any warning sprite will blink.
 
             float transitionBlink = timer / warningDuration;
             float currentBlink = Mathf.Lerp(warningBlinkSlow, warningBlinkFast, transitionBlink);
@@ -385,8 +161,6 @@ public class CosmicRay : MonoBehaviour
             }
             yield return null;
         }
-
-        // set null ? do we want it to vanish?
     }
 
 
@@ -402,10 +176,12 @@ public class CosmicRay : MonoBehaviour
 
             ray.transform.localScale = new Vector3(rayScaleX, fullScaleY, 1f);
         }
+
         if (warning != null)
         {
             warning.gameObject.SetActive(false);
         }
+
         if (topParticles != null)
         {
             Vector3 pos = topParticles.transform.localPosition;
@@ -413,6 +189,7 @@ public class CosmicRay : MonoBehaviour
             topParticles.transform.localPosition = pos;
             topParticles.Play();
         }
+
         yield return PlayAnimation("RayFall");
     }
 
@@ -425,8 +202,10 @@ public class CosmicRay : MonoBehaviour
         {
             rayAnimator.Play("RayLoop", 0, 0f);
         }
+
         yield return new WaitForSeconds(activeDuration);
     }
+
 
     private IEnumerator RayEndingSequence()
     {
@@ -442,7 +221,7 @@ public class CosmicRay : MonoBehaviour
 
         while (elapsed < len)
         {
-            Anchor(); 
+            Anchor(); // This function allows the sprite to "exit" through the bottom of the screen 
             elapsed += Time.deltaTime;
             yield return null;
         }
@@ -467,15 +246,24 @@ public class CosmicRay : MonoBehaviour
     private void RemoveRay()
     {
         killZoneCollider.enabled = false;
-        topParticles.Stop();
+
+        float destroyDelay = 0f;
+
+        if (topParticles != null)
+        {
+            topParticles.Stop();
+            destroyDelay = topParticles.main.duration + topParticles.main.startLifetime.constantMax;
+        }
+        
         if (ray != null)
         {
             ray.gameObject.SetActive(false);
         }
-        Destroy(gameObject);
+
+        Destroy(gameObject, destroyDelay);
     }
 
-    // Trying to use the Unity animator and can't get the animation for the ray to play T-T... using this for now to switch from fall from top to despawn toward bottom
+
     private void Anchor()
     {
         float bottomLocalOffset = ray.sprite.bounds.min.y; 
