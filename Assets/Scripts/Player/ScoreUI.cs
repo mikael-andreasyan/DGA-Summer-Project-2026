352d8f1d753385f81c637e0b9c452494ab1e13eb
jaredrojas08 <jaredrojas08@gmail.com>
2026-07-23 21:23:04 -0400

    new number display
    Signed-off-by: jaredrojas08 <jaredrojas08@gmail.com>

--- Assets/Scripts/Player/ScoreUI.cs
+++ Assets/Scripts/Player/ScoreUI.cs
@@ -6,14 +6,31 @@ public class ScoreUI : MonoBehaviour
 
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private TMP_Text comboText;
+    [SerializeField] private TMP_FontAsset leadDigitFont;
+
+    private int displayedScore = -1;
+    private int displayedCombo = -1;
+
+    private void Awake()
+    {
+        NumberDisplay.RegisterLeadDigitFont(leadDigitFont);
+    }
 
     // Update is called once per frame
     void Update()
     {
         int score = GameManager.Instance.Score;
-        scoreText.text = $"{score}";
+        if (score != displayedScore)
+        {
+            displayedScore = score;
+            scoreText.text = NumberDisplay.Format(score, leadDigitFont);
+        }
 
         int combo = GameManager.Instance.Combo;
-        comboText.text = $"{combo}";
+        if (combo != displayedCombo)
+        {
+            displayedCombo = combo;
+            comboText.text = NumberDisplay.Format(combo, leadDigitFont);
+        }
     }
 }
