f3e67e45dc1fa30d6957a00b66a1b815b9efcc57
jsj84-cornell <jasonjang777@gmail.com>
2026-07-30 22:02:47 -0400

    Jason chunk updates

--- Assets/Scripts/PowerUps/Barrel Launcher.cs
+++ Assets/Scripts/PowerUps/Barrel Launcher.cs
@@ -97,6 +97,7 @@ public class BarrelLauncher : MonoBehaviour
     {
         if (isWaiting)
         {
+            Debug.Log("Barrel time: " + t);
             handlePreLaunch();
         }
     }
@@ -231,6 +232,7 @@ public class BarrelLauncher : MonoBehaviour
             playerRB.AddForce(boostVector, ForceMode2D.Impulse);
             playerController.enabled = true;
             playerController.Launch();
+            exitBarrel();
             Destroy(gameObject);
         }
     }
